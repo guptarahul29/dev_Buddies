506920f1da704a853b132064517799e184c85d7c
guptarahul29 <mr.rahulgupta29@gmail.com>
2025-04-28 21:09:49 +0530

    get users and g userby email

--- src/app.js
+++ src/app.js
@@ -8,34 +8,67 @@ const User =require("./models/user.js");
 
 app.use(express.json());
 
+//find all Users
+app.get("/users", async (req, res)=>{
 
-
-app.post("/signup",async(req,res)=>{
-    
-    console.log(req.body);
-    const user = new User(req.body);
-    
-
-
-
-//     const user =new User({
-//         firstName:"Ram",
-//         lastName:"gg",
-//         age:20,
-//         email:"ram@gmail.com"
-//     }
-// );
     try{
-        await user.save();
-        res.send("data store....");
+        const users =await User.find({});
+        res.send(users);
+        console.log(users);
+        
     }
-    catch{
-        res.status(400).send("erorrrrrr")
+    catch(err){
+        res.status.send("Something went wrong")
     }
-
-    
 
 });
+
+//get users by email id
+app.get("/feed",async (req,res)=>{
+    const emailId = req.body.email;
+    try{
+        // to find only one user which have same email then use ( User.findOne  )
+        const users =await User.find({email : emailId});
+        if(users.length===0){
+            res.send("not found");
+        }
+        else{
+            res.send(users);
+            console.log(users);
+        }
+        
+        
+    }
+    catch(err){
+            res.send("Something wend wrong")
+    }
+})
+
+
+
+// app.post("/signup",async(req,res)=>{
+    
+//     console.log(req.body);
+//     const user = new User(req.body);
+    
+// //     const user =new User({
+// //         firstName:"Ram",
+// //         lastName:"gg",
+// //         age:20,
+// //         email:"ram@gmail.com"
+// //     }
+// // );
+//     try{
+//         await user.save();
+//         res.send("data store....");
+//     }
+//     catch{
+//         res.status(400).send("erorrrrrr")
+//     }
+
+    
+
+// });
  
 
 
